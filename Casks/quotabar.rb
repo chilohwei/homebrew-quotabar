e66667bb0b36d9b9bdb068ba74e04751d5a26686
chilohwei <chilohwei@gmail.com>
2026-04-27 22:52:40 +0800

    Remove redundant verified field

--- Casks/quotabar.rb
+++ Casks/quotabar.rb
@@ -2,8 +2,7 @@ cask "quotabar" do
   version "1.1.3"
   sha256 "34f3c6b497295ceaf3604e6a96c4367fff4d49df9f2ecbf9c632867e8d4f04c4"
 
-  url "https://github.com/chilohwei/QuotaBar/releases/download/v#{version}/QuotaBar-#{version}-universal.dmg",
-      verified: "github.com/chilohwei/QuotaBar/"
+  url "https://github.com/chilohwei/QuotaBar/releases/download/v#{version}/QuotaBar-#{version}-universal.dmg"
   name "QuotaBar"
   desc "Menu bar tool for viewing Codex quota and switching local accounts"
   homepage "https://github.com/chilohwei/QuotaBar"
