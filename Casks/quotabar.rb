4ca35612ebcc787b856d2d47f0f65bd896727965
chilohwei <chilohwei@gmail.com>
2026-05-14 22:42:43 +0800

    Add QuotaBar cask

---
+++ Casks/quotabar.rb
@@ -0,0 +1,23 @@
+cask "quotabar" do
+  version "1.0.0"
+  sha256 "ecc0962badb3c358fe6934514dd5b2886817d0955b144f35af441d30d6d56f1c"
+
+  url "https://github.com/chilohwei/QuotaBar/releases/download/v#{version}/QuotaBar-#{version}-universal.dmg"
+  name "QuotaBar"
+  desc "Menu bar tool for viewing AI tool quotas and switching local accounts"
+  homepage "https://github.com/chilohwei/QuotaBar"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "QuotaBar.app"
+
+  zap trash: [
+    "~/Library/Application Support/QuotaBar",
+    "~/Library/Preferences/com.chiloh.QuotaBar.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# homebrew-quotabar
+
+Homebrew tap for [QuotaBar](https://github.com/chilohwei/QuotaBar).
+
+## Install
+
+```bash
+brew tap chilohwei/quotabar
+brew install --cask quotabar
+```
+
+## Upgrade
+
+```bash
+brew update
+brew upgrade --cask quotabar
+```
+
+## Uninstall
+
+```bash
+brew uninstall --cask quotabar
+```
